a6ccb2bd629cb379014058884e2593b8c59b2116
Minh Pham <minh32520@gmail.com>
2026-06-01 20:56:57 +0700

    fix: ignore build files and local database

--- apps/backend/target/classes/db/migration/V3__add_mfa_attempts_to_auth_tokens.sql
+++
@@ -1,6 +0,0 @@
--- UC-35 BR-35-05: track wrong TOTP entries per TFA_TEMP token (max 5 before revoke)
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns
-    WHERE object_id = OBJECT_ID('auth_tokens') AND name = 'mfa_attempts'
-)
-    ALTER TABLE auth_tokens ADD mfa_attempts INT NOT NULL DEFAULT 0;
